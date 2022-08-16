6f1f207e897750c3bb9ec5bf218e5add02db1f67
Giovanni Ceretti <giovanniceretti@Giovannis-MacBook-Pro.local>
2022-08-16 23:33:14 +0100

    Final Submission

--- app/main_app.py
+++ app/main_app.py
@@ -11,64 +11,66 @@ Created by Giovanni Ceretti
 st.write("""
 """)
 st.write("""
-This is a Streamlit web app created so users can generate a prediction for the price of a used car. The model is based off August 2022 used car listing data from the British online car reseller, Cazoo.
-""")
+This is a Streamlit web app created for users to generate a prediction for the price of a used car. The model is based off August 2022 used car listing data from the British online car reseller, Cazoo. If you run the rest of the code in this project, you can generate a more recent set of data to generate predictions off.""")
 st.write("""
 """)
 st.write("""
-Please use the input boxes below to input the desired feature characteristics; input values for all features.
+Please use the complete the feature prompts below by inputting your desired vehicle characteristics.
 """)
 
 # Creating the categorical variable input list
-car_make_list = pickle.load(open('./model/car_make_list.pkl','rb'))
-model_list = pickle.load(open('./model/model_list.pkl','rb'))
+make_model_dict = pickle.load(open('./model/make_model_dict.pkl','rb'))
+model_trim_dict = pickle.load(open('./model/model_trim_dict.pkl','rb'))
 transmission_list = pickle.load(open('./model/transmission_list.pkl','rb'))
-trim_list = pickle.load(open('./model/trim_list.pkl','rb'))
 color_list = pickle.load(open('./model/color_list.pkl','rb'))
 
 # Creating the input feature boxes
 
 car_make = st.selectbox(
     'Car Make',
-['<Select Make>'] + car_make_list
+options = ['<Select Make>'] + list(make_model_dict.keys())
 )
 
-car_model = st.selectbox(
-    'Car Model',
-    ['<Select Model>'] + model_list
+if car_make != '<Select Make>':
+    car_model = st.selectbox(
+        'Car Model',
+        options = ['<Select Model>'] + make_model_dict[car_make]
 )
 
-trim = st.selectbox(
-    'What trim is on the car?',
-    ['<Select Trim>'] + trim_list
+if car_model != '<Select Model>':
+    trim = st.selectbox(
+        'What trim is on the car?',
+        options = ['<Select Trim>'] + model_trim_dict[car_model]
 )
 
-reg_date = st.selectbox(
-    'Car Registration Year',
-    [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014]
-)
+if trim != '<Select Trim>':
+    reg_date = st.selectbox(
+        'Car Registration Year',
+        ['<Select Registration Year>',2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014]
+        )
 
-car_age = 2022 - reg_date
-if car_age == 0:
+if isinstance(reg_date, int):
+    car_age = 2022 - reg_date
+    if car_age == 0:
         car_age = 0.5
-car_age = car_age ** 0.5
+    car_age = car_age ** 0.5
 
+if reg_date != '<Select Registration Year>':
+        transmission = st.selectbox(
+        'Manual or Automatic',
+        ['<Select Transmission>'] + transmission_list
+        )
 
-mileage = st.number_input(
-    'Car Mileage',
-    step=1000
-)
-
-transmission = st.selectbox(
-    'Manual or Automatic',
-    ['<Select Transmission>'] + transmission_list
-)
-
-color = st.selectbox(
-    'What color is the car?',
-    ['<Select Color>'] + color_list
-)
-
+if transmission != '<Select Transmission>':
+    color = st.selectbox(
+        'What color is the car?',
+        ['<Select Color>'] + color_list
+        )
+if color != '<Select Color>':
+        mileage = st.number_input(
+        'Car Mileage',
+        step=1000
+        )
 
 # Load pickled encoder
 encoder = pickle.load(open('./model/input_encoder.pkl','rb'))
